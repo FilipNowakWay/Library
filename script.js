a06a2513196662a46f7496ed4b5029728628fbfc
Filip Nowak <filipnowakkoscian@gmail.com>
2026-09-22 12:38:50 +0200

    Add book display rendering and read status handling
    - Fix DOM selector for library display container using ID

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const myLibrary = [];
 
-function Book(id, title, author, pages, year) {
+function Book(id, title, author, pages, status) {
   this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.status = status;
 }
 
-function createAndAddBook(title, author, pages) {
+function createAndAddBook(title, author, pages, status) {
   const id = crypto.randomUUID();
 
-  const newBook = new Book(id, title, author, pages);
+  const newBook = new Book(id, title, author, pages, status);
 
   myLibrary.push(newBook);
   console.log(myLibrary);
@@ -18,9 +19,54 @@ function createAndAddBook(title, author, pages) {
 document.querySelector("#book-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const title = document.querySelector("#title");
-  const author = document.querySelector("#author");
-  const pages = document.querySelector("#pages");
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
 
-  createAndAddBook(title, author, pages);
+  //
+  //bookstatus
+  //
+
+  const statusCheckbox = document.querySelector("#status");
+  const isFinished = statusCheckbox.checked;
+  const status = isFinished ? "Finished" : "Not finished yet";
+
+  createAndAddBook(title, author, pages, status);
+  displayBooks();
 });
+
+// ------------------
+// Display Logic
+// ------------------
+
+const libraryDisplay = document.querySelector("#library-display");
+function displayBooks() {
+  libraryDisplay.innerHTML = "";
+
+  myLibrary.forEach((book, index) => {
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+
+    card.dataset.index = index;
+
+    //Create and fill elements for the card:
+    const title = document.createElement("h3");
+    title.textContent = book.title;
+
+    const author = document.createElement("p");
+    author.textContent = `By: ${book.author}`;
+
+    const pages = document.createElement("p");
+    pages.textContent = `${book.pages} pages`;
+
+    const status = document.createElement("p");
+    status.textContent = book.status;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(status);
+
+    libraryDisplay.appendChild(card);
+  });
+}
